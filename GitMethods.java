92bd0445e9d47583e9be7f54bc5bade42f1a7ba4
loganpeoplesclick <loganashok666@gmail.com>
2022-09-20 22:15:26 +0530

    new files

---
+++ GitMethods.java
@@ -0,0 +1,24 @@
+package org.green.test.git;
+
+public class GitMethods
+{
+
+	private void ClassA() 
+	{
+	  System.out.println("I am in Class A");
+
+	}
+	private void ClassB() 
+	{
+		
+		 System.out.println("I am in Class B");
+	}
+	private void ClassC() 
+	{
+		 System.out.println("I am in Class C");
+
+	}
+	
+	
+	
+}
